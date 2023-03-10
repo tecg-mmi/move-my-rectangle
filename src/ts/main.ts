3edb91b492ef0a43df0cd2ea4a0d2ed8216065f3
Daniel Schreurs <daniel.schreurs@hotmail.com>
2023-03-10 09:55:29 +0100

    :tada: Initial commit

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,11 +1,19 @@
 import {Rectangle} from "./shapes/Rectangle";
 import {Hsl} from "./Colors/Hsl";
 
+// @ts-ignore
+const canvas: HTMLCanvasElement = document.getElementById('my-canvas');
+const myRectangle = new Rectangle(canvas, new Hsl(120, 80, 40), {x: 40, y: 100}, 20, 50);
+
 function main(): void {
-    // @ts-ignore
-    const ctx: CanvasRenderingContext2D = document.getElementById('my-canvas').getContext('2d');
-    const myRectangle = new Rectangle(ctx, new Hsl(120, 80, 40), {x: 40, y: 100}, 20, 50);
-    myRectangle.draw();
+    animate();
 }
 
 main();
+
+function animate() {
+    myRectangle.clear();
+    myRectangle.update();
+    myRectangle.draw();
+    requestAnimationFrame(animate);
+}
