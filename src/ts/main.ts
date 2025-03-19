39a17aa985efc98c7f1c02332e148a7a093e573d
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2025-03-19 11:16:58 +0100

    Add Math.atan2

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,7 +1,7 @@
 import {Rectangle} from "./framework25/shapes/Rectangle";
 import {settings} from "./settings";
 import {Hsl} from "./framework25/colors/Hsl";
-import {randomInt} from "./framework25/helpers/random";
+import {randomFloat, randomInt} from "./framework25/helpers/random";
 
 const app = {
     init() {
@@ -15,6 +15,7 @@ const app = {
             new Hsl(200, 40, 60),
             settings.rect.width,
             settings.rect.height,
+            randomFloat(0, Math.PI * 2)
         );
 
         this.myRect.draw();
@@ -29,6 +30,15 @@ const app = {
         window.addEventListener('resize', () => {
             this.resizeCanvas();
         });
+        window.addEventListener('mousemove', (event) => {
+            // update rotation :
+
+            const dx = event.clientX - this.myRect.position.x;
+            const dy = event.clientY - this.myRect.position.y;
+
+            this.myRect.rotation = Math.atan2(dy, dx);
+
+        })
     },
     resizeCanvas() {
         this.canvas.width = window.innerWidth;
@@ -36,7 +46,11 @@ const app = {
     },
     update() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.myRect.position.x++;
+
+        this.myRect.position.x += Math.cos(this.myRect.rotation) * settings.step;
+        this.myRect.position.y += Math.sin(this.myRect.rotation) * settings.step;
+
+        // TODO : Check Y
         if (this.myRect.position.x > this.canvas.width + this.myRect.width / 2) {
             this.myRect.position.x = -this.myRect.width / 2;
             this.myRect.position.y = randomInt(this.myRect.height / 2, this.canvas.height - this.myRect.height / 2);
